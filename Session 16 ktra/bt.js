a63b1f3daef2f791becb698a9abdb8cc9051c77c
MinhMario <minom35w@gmail.com>
2026-03-06 15:53:36 +0700

    Update bt.js

--- Session 16 ktra/bt.js
+++ Session 16 ktra/bt.js
@@ -1,10 +1,10 @@
-
-
-function addUser(){
-    let password=document.getElementById("pass").value;
+let user=[];
+function addUser(event){
+    event.preventDefault();
+let password=document.getElementById("pass").value;
 let email=document.getElementById("email").value;
 let repass=document.getElementById("repass").value;
-let user=[];
+
 if(repass!==password){
     alert("Mật khẩu và nhập lại không trùng nhau")
 }
